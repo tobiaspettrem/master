fa3e09830866a3e9598ffbc9dfd718b97a6db11c
tobiaspettrem <tobias.pettrem@gmail.com>
2018-03-22 19:13:25 +0100

    exciting times

--- virdi.py
+++ virdi.py
@@ -1,25 +1,43 @@
-import alva_io, address, pandas as pd, numpy as np
+import alva_io, address
+import pandas as pd
+import numpy as np
+import calendar
+from patsy import dmatrices
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
-virdi_df = alva_io.get_dataframe_from_excel("C:/Users/tobiasrp/data/20180220 Transactions Virdi v2.xlsx")
+SOLD_TO_OFFICIAL_DIFF = 55
 
-address_df = address.get_address()
+run_new_init = False
 
-virdi_augmented = pd.merge(virdi_df, address_df, on="id")
+if raw_input("Enter to run without new initialization") != "":
+    run_new_init = True
 
+if run_new_init:
+
+    virdi_df = alva_io.get_dataframe_from_excel("C:/Users/tobiasrp/data/20180220 Transactions Virdi v2.xlsx")
+
+    address_df = address.get_address()
+
+    virdi_augmented = pd.merge(virdi_df, address_df, on="id")
+
+    print(virdi_augmented.head())
+    print(virdi_augmented.info())
+    print(virdi_augmented.isnull().sum())
+
+    print virdi_augmented.shape
+    print("Dropping duplicates")
+    virdi_augmented = virdi_augmented.drop_duplicates(subset="ad_code", keep=False)
+
+    pd.DataFrame.to_csv(virdi_augmented, "C:/Users/tobiasrp/data/virdi_augmented.csv")
+
+else:
+    virdi_augmented = pd.read_csv("C:/Users/tobiasrp/data/virdi_augmented.csv")
+
+print virdi_augmented.shape
 print(virdi_augmented.head())
 print(virdi_augmented.info())
 print(virdi_augmented.isnull().sum())
-
-print virdi_augmented.shape
-print("Dropping duplicates")
-virdi_augmented = virdi_augmented.drop_duplicates(subset="ad_code", keep=False)
-print virdi_augmented.shape
-
-print(virdi_augmented.head())
-print(virdi_augmented.info())
-print(virdi_augmented.isnull().sum())
-
-pd.DataFrame.to_csv(virdi_augmented, "C:/Users/tobiasrp/data/virdi_augmented.csv")
 
 # virdi = virdi.loc[virdi["kr/m2"] > ] # cutting on sqm price
 
@@ -28,7 +46,7 @@ virdi_augmented = virdi_augmented.assign(log_price_plus_comdebt = np.log(virdi_a
 # ----- CREATE NEW COLUMN size_group ------ #
 
 size_group_size = 10 #change this only to change group sizes
-size_labels = ["10 - 29"]
+size_labels = ["10 -- 29"]
 size_labels += [ "{0} - {1}".format(i, i + size_group_size - 1) for i in range(30, 150, size_group_size) ]
 size_labels.append("150 - 179")
 size_labels.append("180 - ")
@@ -40,38 +58,96 @@ virdi_augmented['size_group'] = pd.cut(virdi_augmented.prom, size_thresholds, ri
 
 print virdi_augmented.head(10)
 
+#----------- CREATE NEW COLUMN real_sold_date -----------#
+
+#makes sold_date on datetime format
+virdi_augmented = virdi_augmented.assign(sold_date = pd.to_datetime(virdi_augmented["sold_date"],format="%Y-%m-%d"))
+
+#makes off._date on datetime format
+virdi_augmented = virdi_augmented.assign(official_date = pd.to_datetime(virdi_augmented["official_date"],format="%Y-%m-%d"))
+
+#sets new col real_sold_date equal to minimum of sold_date and official_date (with offset of x days)
+virdi_augmented = virdi_augmented.assign(real_sold_date = np.where((virdi_augmented["sold_date"] > virdi_augmented["official_date"]), \
+                                                                   virdi_augmented["official_date"] - pd.Timedelta(days = SOLD_TO_OFFICIAL_DIFF), \
+                                                                   virdi_augmented["sold_date"]))
+
+
+#sets real_sold_date equal to official_date if sold_date is NaT (not a problem in the opposite case for some reason)
+virdi_augmented = virdi_augmented.assign(real_sold_date = np.where((virdi_augmented["real_sold_date"].isnull()), \
+                                                                   virdi_augmented["official_date"]- pd.Timedelta(days = SOLD_TO_OFFICIAL_DIFF), \
+                                                                   virdi_augmented["real_sold_date"]))
+
+#----- CREATE NEW COLUMN sold_month ------#
+
+#add sold_month column for discovery of seasonal effects
+
+virdi_augmented = virdi_augmented.assign(sold_month = virdi_augmented.real_sold_date.map(lambda x: x.month))
+virdi_augmented = virdi_augmented.assign(sold_year = virdi_augmented.real_sold_date.map(lambda x: x.year))
+virdi_augmented = virdi_augmented.assign(sold_month = virdi_augmented.sold_month.apply(lambda x: calendar.month_abbr[x])) #text instead of int
+virdi_augmented = virdi_augmented.assign(sold_month_and_year = virdi_augmented.sold_month + "_" + virdi_augmented.sold_year.map(str))
+
+#delete sold_date and official_date now that real_sold_date covers both
+del virdi_augmented["sold_date"]
+del virdi_augmented["official_date"]
+del virdi_augmented["register_date"]
+
 
 # ------------------
 #   DO REGRESSION
 # ------------------
 
 virdi_augmented = virdi_augmented.sample(frac=1) # shuffle the dataset to do random training and test partition
+virdi_augmented = virdi_augmented[["log_price_plus_comdebt","size_group","sold_month_and_year","bydel_code","unit_type", "prom","Total price","coord_x","coord_y"]]
+
+columns_to_count = ["log_price_plus_comdebt","size_group","sold_month_and_year","bydel_code","unit_type"]
+
+virdi_augmented = virdi_augmented.loc[virdi_augmented.bydel_code != "SEN"]
+virdi_augmented = virdi_augmented.loc[virdi_augmented.bydel_code != "MAR"]
+
+virdi_augmented = virdi_augmented.loc[virdi_augmented.sold_month_and_year != "Feb2018"]
+virdi_augmented = virdi_augmented.loc[virdi_augmented.sold_month_and_year != "Jan2018"]
+
+"""
+for c in columns_to_count:
+    print(c)
+    print(virdi_augmented[c].value_counts())
+    print("-------------------")
+"""
 
 threshold = int(0.8 * len(virdi_augmented))
 training = virdi_augmented[:threshold]
 test = virdi_augmented[threshold:]
 
 y,X = dmatrices('log_price_plus_comdebt ~ C(size_group,Treatment(reference="40 - 49")) + \
-sold_month + C(bydel_code,Treatment(reference="bfr")) + unit_type', \
+C(sold_month_and_year,Treatment(reference="Apr2017")) + C(bydel_code,Treatment(reference="bfr")) + \
+C(unit_type,Treatment(reference="house"))', \
                 data=training, return_type = "dataframe") # excluding build year until videre
 
-"""
 
 #describe model
+"""
 print("y")
 print(y.head())
 print("x")
 print(X.head())
 print("---")
+"""
 
 mod = sm.OLS(y,X,missing = 'drop')
 
 #fit model
 res = mod.fit()
 
-# resids = res.resid
-# plt.scatter(resids.index,resids)
-# plt.show()
+resids = res.resid
+
+virdi_with_resids = training
+virdi_with_resids["resids"] = resids
+
+alva_io.write_to_csv(virdi_with_resids,"C:/Users/tobiasrp/data/virdi_with_resids.csv")
+
+"""
+#plt.scatter(resids.index,resids)
+#plt.show()
 
 #magic
 print(res.summary())
@@ -83,7 +159,8 @@ print(res.HC0_se)
 # exogen_matrix = [c for c in subset.columns if c not in regression_columns]
 
 y_test,X_test = dmatrices('log_price_plus_comdebt ~ C(size_group,Treatment(reference="40 - 49")) + \
-sold_month + C(bydel,Treatment(reference="FROGNER")) + year_group + bygningstype', \
+C(sold_month_and_year,Treatment(reference="Apr2017")) + C(bydel_code,Treatment(reference="bfr")) + \
+C(unit_type,Treatment(reference="house"))', \
                 data=test, return_type = "dataframe")
 
 
@@ -93,8 +170,8 @@ print("Test Results")
 
 score = pd.DataFrame()
 
-score = score.assign(predicted_value = (test.prom*np.exp(test_results) - test.common_debt)).astype(int)
-score = score.assign(true_value = test.sold_price)
+score = score.assign(predicted_value = (test.prom*np.exp(test_results))).astype(int)
+score = score.assign(true_value = test["Total price"])
 score = score.assign(deviation_absolute = score.true_value - score.predicted_value)
 score = score.assign(deviation_percentage = abs(score.deviation_absolute / score.true_value))
 
@@ -109,5 +186,4 @@ print(score.quantile([0.25,0.5,0.75]))
 # fig = sm.graphics.plot_partregress("sqmeter_price","prom",["sold_month","ordinal_sold_date","bydel"],data=subset, obs_labels=False)
 # fig.show()
 # time.sleep(10)
-
 """
